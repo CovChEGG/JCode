358922f0199bcf9896ace958a5f44aa08bf5f63d
Pavel Glukhov <covchegg@gmail.com>
2022-07-28 18:49:26 +0700

    Реализована функция восстановления пути с выводом

--- HomeWorks/hw005/wave.java
+++ HomeWorks/hw005/wave.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class wave {
     public static void main(String[] args) {
         
-        int x=5,    //  раземеры карты
-            y=10;
-        int obstacles = 5; // (от 1 до 9) - относительное количество            
+        int x=10,    //  раземеры карты
+            y=20;
+        int obstacles = 5; // (от 0 до 9) - относительное количество препядствий           
         int[] startEnd = {0, 0, 0, 0}; // массив для передачи начальных и конечных координат
 
         int[][] map = new int[x][y]; // инициализация карты
@@ -15,39 +15,82 @@ public class wave {
         fillTheMap(map, obstacles); // заполнение карты начальными условиями
         PrintMap(map, "Заполенная карта препядствиями (-2) и начальной (1) и конечной (-1) точками");
         userInput.nextLine();
+        
         startEnd = findMarkers(map, startEnd); // определение координат начала и конца
-        waveS(map, startEnd); 
+        if(waveS(map, startEnd)) { // запуск волны и печать результата
+            userInput.nextLine();
+            wayBack(map, startEnd); // если путь найден - вывести о нём информацию
+        }; 
         userInput.nextLine();
-
-        // wayBack(map, s  tartEnd);
         // PrintMap(map);
+        userInput.close();
+    }
 
+    public static void wayBack (int[][]map, int[] startEnd) {
+        int maxX = map.length;
+        int maxY = map[0].length;
+        ArrayDeque<Integer> qX = new ArrayDeque<>();
+        ArrayDeque<Integer> qY = new ArrayDeque<>();
+        int sX = startEnd[0];
+        int sY = startEnd[1];
+        int eX = startEnd[2];
+        int eY = startEnd[3];
+        qX.add(eX);
+        qY.add(eY);
+        map[eX][eY] *= -1;
+        int[] moveX = {-1, 0, 1 ,0}; // окресность фон Неймана
+        int[] moveY = {0, 1, 0, -1};
+        int cX = eX;
+        int cY = eY;
+        int cN = map[eX][eY];
+        map[eX][eY] *= 10;
+        int x = 0;
+        int y = 0;
+        int n = 0;
+        boolean notStart = true;
+        while(notStart) {
+            for(int i = 0; i <4 ; i++) {  
+                x = cX - moveX[i];
+                y = cY - moveY[i];
+                n = cN - 1;
+                if(x >= 0 && x < maxX && y >= 0 && y < maxY && (map[x][y] == n)) {
+                    map[x][y] *= 10;
+                    cX = x;
+                    cY = y;
+                    cN = n;
+                    qX.addFirst(cX);
+                    qY.addFirst(cY);
+                    if (x == sX && y == sY) notStart = false;
+                    break;
+                }
+            }
+        }
+        StringBuilder way = new StringBuilder();
+        while(!qX.isEmpty()) way.append("(" + qX.pollFirst() + ", " + qY.pollFirst() + ")");
+        PrintMap(map, "Путь на карте умножен на 10, координаты движения: \n" + way);
     }
 
     public static void fillTheMap(int[][] map, int obs) {
+        if(obs>9 || obs < 0) {
+            System.out.println("Неправильный коэффициент препядствий! Он может быть от 0 до 9");
+            System.exit(0);
+        }
         int maxX = map.length;
         int maxY = map[0].length;
         int x, y;
         int obSnum = maxX * maxY * obs / 10;
         int i = 0;
-        while (i < (obSnum + 1)) {
+        while (i <= (obSnum + 1)) {
             x = (int) (Math.random() * maxX);
             y = (int) (Math.random() * maxY);
-            i++;
             if(map[x][y] == 0) {
                 if(i < obSnum) map[x][y] = -2; // установка препядствий
                 else if(i == obSnum) map[x][y] = 1; // установка начала
                 else map[x][y] = -1; // установка конца
+                i++;
             }
-            else i--;
         }
     }
-
-    // public static void wayBack (int[][]map, int[] startEnd) {
-    //     int maxX = map.length;
-    //     int maxY = map[0].length;
-
-    // }
 
     public static int[] findMarkers(int[][] map, int[] startEnd) {
         int maxX = map.length;
@@ -129,11 +172,11 @@ public class wave {
         System.out.println(text + "\n\n");
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[0].length; j++) {
-                System.out.printf("%4d ", map[i][j]);
+                System.out.printf("%3d ", map[i][j]);
             }
             System.out.println();
             System.out.println();
         }
-        System.out.println("Нажмите 'Enter' чтобы подолжить или 'Ctrl' + 'C', чтобы выйти");
+        System.out.println("Нажмите 'Enter' чтобы продолжить");
     }
 }
