97bb69d8b4b6df2dcb64644ff2a692de16ebf8d9
Pavel Glukhov <covchegg@gmail.com>
2022-08-02 22:40:15 +0700

    First minds about hw006

---
+++ HomeWorks/hw006/calcWithQestions.java
@@ -0,0 +1,78 @@
+/*
+Задано уравнение вида q + w = e, q, w, e >= 0.
+Некоторые цифры могут быть заменены знаком вопроса, например 2? + ?5 = 69.
+Требуется восстановить выражение до верного равенства.
+Предложить хотя бы одно решение или сообщить, что его нет.
+ */
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class calcWithQestions {
+    public static void main(String[] args) {
+        String exp = "2?+?5=69";
+        exp = exp.trim().replace(" ", "");
+        System.out.print("\033[H\033[J");
+        System.out.println(exp);
+        List<String> ls = knwnNmbr(exp);
+        System.out.println(ls);
+        // List<String> ls1 = parsNmbr(ls);
+        // System.out.println(ls1);
+
+    }
+
+    public static List<Integer> parsNmbr(List<String> str) {
+        List<Integer> res = new ArrayList<>();
+
+        // for (String item:str) {
+        // res.add(Character.toString(item.charAt(item.length()-1)));
+        // }
+        return res;
+    }
+
+    public static List<String> knwnNmbr(String s){
+        HashSet<Character> signs = new HashSet<>(Arrays.asList('+', '='));
+        List<String> parts = new List<String>(); 
+        parts = 
+        Double[] data = new Double[9];
+        String res = "";
+        String tmp = "";
+        boolean rightPart = true;
+        int i = 0,
+            length = s.length(),
+            qI = 0,
+            dI = 0;
+        while(i < length){
+            c = 0;
+            if(s.charAt(i) == '=') rightPart = false;
+            while (!signs.contain(s.charAt(i)) || i != length) { 
+                    if
+                    data[dI+1] += 1; // сколько в текущем числе знаков ?
+                    tmp += 0;
+                    c++;
+                } else if(Character.isDigit(s.charAt(i)) || s.charAt(i) == '.') {
+                        tmp += s.charAt(i);
+                } else {
+                    System.out.println("Не правильный символ в выражении: " + s.charAt(i));
+                    System.exit(0);
+                }            
+                if(data[dI+1]) {
+                    data[dI+2] = i - c;
+                }
+                data[dI] =       
+                i++;
+            }
+            if(s.charAt(i) == '=') 
+
+
+        }
+
+                result.add(res);
+                res="";
+        result.add(res);return result;
+    }
+
+    public static 
+    
+}
