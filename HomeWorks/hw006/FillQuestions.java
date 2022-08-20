356c3691102685458391168da147d88e0eb20c0d
Pavel Glukhov <covchegg@gmail.com>
2022-08-21 00:35:26 +0700

    Added examples for testing

--- HomeWorks/hw006/FillQuestions.java
+++ HomeWorks/hw006/FillQuestions.java
@@ -8,8 +8,14 @@
 
 public class FillQuestions {
     public static void main(String[] args) {
-        String exp = "1??+?45=3?6";
+        // String exp = "1??+?45=3?6";
         // String exp = "1??+?45=326";
+        // String exp = "2? + ?5 = 69";
+        // String exp = "2? + ?5 = ??";
+        // String exp = "2? + ?5 = 69";
+        // String exp = "2? + ?5 = 19";
+        // String exp = "2? + ?5 = 119";
+        String exp = "?7? + ?1 = 41?";
         Restoration reExp = new Restoration(exp);
         System.out.println(reExp.GetRes() + "\n");
     }
